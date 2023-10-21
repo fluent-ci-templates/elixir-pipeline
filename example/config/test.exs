b00c8b9f16b99da181d4f4407db9a10be9487bdb
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-21 09:54:03 +0000

    use pkgx instead of devbox

--- example/config/test.exs
+++ example/config/test.exs
@@ -6,9 +6,9 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :example, Example.Repo,
-  username: "root",
-  password: "",
-  hostname: "localhost",
+  username: System.get_env("MYSQL_USER") || "root",
+  password: System.get_env("MYSQL_ROOT_PASSWORD") || "",
+  hostname: System.get_env("MYSQL_HOST") || "localhost",
   database: "example_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
--- src/dagger/jobs.ts
+++ src/dagger/jobs.ts
@@ -8,42 +8,39 @@ export const exclude = [".git", ".devbox", "deps", "_build"];
 
 export const test = async (src = ".") => {
   await connect(async (client: Client) => {
+    const mysql = client
+      .container()
+      .from("mysql")
+      .withEnvVariable("MYSQL_ROOT_PASSWORD", "pass")
+      .withEnvVariable("MYSQL_DATABASE", "example_test")
+      .withExposedPort(3306);
+
     const context = client.host().directory(src);
     const baseCtr = client
       .pipeline(Job.test)
       .container()
-      .from("ghcr.io/fluentci-io/devbox:latest")
-      .withExec(["mv", "/nix/store", "/nix/store-orig"])
-      .withMountedCache("/nix/store", client.cacheVolume("nix-cache"))
-      .withExec(["sh", "-c", "cp -r /nix/store-orig/* /nix/store/"]);
+      .from("pkgxdev/pkgx:latest")
+      .withExec(["apt-get", "update"])
+      .withExec(["apt-get", "install", "-y", "ca-certificates"])
+      .withExec(["pkgx", "install", "elixir", "mix"]);
 
     const ctr = baseCtr
+      .withServiceBinding("mysql", mysql)
+      .withEnvVariable("MYSQL_ROOT_PASSWORD", "pass")
+      .withEnvVariable("MYSQL_DATABASE", "example_test")
+      .withEnvVariable("MYSQL_HOST", "mysql")
       .withDirectory("/app", context, { exclude })
       .withWorkdir("/app")
       .withMountedCache("/root/.mix", client.cacheVolume("mix"))
       .withMountedCache("/app/deps", client.cacheVolume("deps"))
       .withMountedCache("/app/_build", client.cacheVolume("_build"))
-      .withExec([
-        "sh",
-        "-c",
-        "mkdir -p .devbox && eval $(devbox shell --print-env) && \
-       devbox services up -b && \
-       devbox services stop && \
-       sed -i 's/mysqld 2/mysqld --user=root 2/' .devbox/virtenv/mysql80/process-compose.yaml",
-      ])
-      .withExec([
-        "sh",
-        "-c",
-        "devbox services up -b && \
-       sleep 3 && \
-       eval $(devbox shell --print-env) && \
-       mix local.rebar --force && \
-       mix local.hex --force && \
-       HEX_HTTP_CONCURRENCY=1 HEX_HTTP_TIMEOUT=120 mix deps.get && \
-       mix ecto.create && \
-       mix test && \
-       devbox services stop",
-      ]);
+      .withExec(["mix", "local.rebar", "--force"])
+      .withExec(["mix", "local.hex", "--force"])
+      .withEnvVariable("HEX_HTTP_CONCURRENCY", "1")
+      .withEnvVariable("HEX_HTTP_TIMEOUT", "120")
+      .withExec(["mix", "deps.get"])
+      .withExec(["mix", "ecto.create"])
+      .withExec(["mix", "test"]);
 
     const result = await ctr.stdout();
 
